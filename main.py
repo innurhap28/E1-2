708fe54f580e5171c1ee34e801af89805263a4c9
innurhap28 <kmugi118@gmail.com>
2026-04-21 19:43:24 +0900

    feat: EOFError, KeyboardInterrupt 예외 처리

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from quiz_game import QuizGame
 
 if __name__ == "__main__":
-    game = QuizGame()
-    game.run()
+    try:
+        game = QuizGame()
+        game.run()
+    except (EOFError, KeyboardInterrupt):
+        print("\n프로그램을 저장 후 종료합니다")
+        game.save_state()
